7301959e4b8eebd766fa93a01a7d174268b88397
ALSK <alsk@nykredit.dk>
2020-03-18 09:24:34 +0100

    various updates

---
+++ src/main/java/com/skov/MailClient.java
@@ -0,0 +1,61 @@
+package com.skov;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailClient {
+
+    static Properties mailServerProperties;
+    static Session getMailSession;
+    static MimeMessage generateMailMessage;
+
+    public static void main(String[] args) {
+        generateAndSendEmail(new StringBuffer("test test og atter test"));
+    }
+
+    public static StringBuffer generateAndSendEmail(StringBuffer mailTxt) {
+
+        try {
+
+            String subject;
+            if (mailTxt.toString().contains("TAKE THE BIKE")) {
+                subject = "YrBike: TAKE THE BIKE dude :)";
+            } else {
+                subject = "YrBike: no need to bicycle to work today dude";
+            }
+
+            mailTxt.append("Welcome to MailClient.generateAndSendEmail()<br>\n");
+
+            mailServerProperties = System.getProperties();
+            mailServerProperties.put("mail.smtp.port", "587");
+            mailServerProperties.put("mail.smtp.auth", "true");
+            mailServerProperties.put("mail.smtp.starttls.enable", "true");
+            //System.out.println("Mail Server Properties have been setup successfully..");
+
+            getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+            generateMailMessage = new MimeMessage(getMailSession);
+
+            generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("allan.noergaard.skov@gmail.com"));
+            generateMailMessage.setSubject(subject);
+            generateMailMessage.setContent(mailTxt + "<br>\n<br>\nhttps://www.yr.no/place/Denmark/North_Jutland/Aalborg/", "text/html");
+
+            Transport transport = getMailSession.getTransport("smtp");
+
+            transport.connect("smtp.gmail.com", "allan.noergaard.skov@gmail.com", "suxligxvuvjlrpdr\n");
+            transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+            transport.close();
+
+            mailTxt.append("mail send. Thanks<br>\n");
+
+        } catch (Exception e) {
+            mailTxt.append("Exception sending mail: " + e.toString() + "<br>\n");
+        } finally {
+            return mailTxt;
+        }
+    }
+}
---
+++ src/main/java/com/skov/Main.java
@@ -0,0 +1,108 @@
+package com.skov;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import com.sun.org.apache.xerces.internal.dom.DeferredElementNSImpl;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Main {
+
+    static StringBuffer mailTxt = new StringBuffer("Welcome<br>\n");
+
+
+    public static void main(String[] args) throws Exception {
+        doExecute();
+        //getRainSumHoursOfInterest(getYrDataXml("https://www.yr.no/place/Denmark/North_Jutland/Aalborg/forecast_hour_by_hour.xml"));
+    }
+
+
+    private static Document getYrDataXml(String url) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        return factory.newDocumentBuilder().parse(new URL(url).openStream());
+    }
+
+    public static String doExecute() {
+        mailTxt = new StringBuffer("Welcome! LocalDateTime.now()=" + LocalDateTime.now() + "<br><br>\n");
+
+        try {
+
+            int dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK); //starter med søndag = 1
+            mailTxt.append("dayOfWeek=" + dayOfWeek + "<br>\n");
+
+            Double rainSumHoursOfInterest =
+                getRainSumHoursOfInterest(getYrDataXml("https://www.yr.no/place/Denmark/North_Jutland/Aalborg/forecast_hour_by_hour.xml"));
+
+            if (dayOfWeek == 6 || dayOfWeek == 7) { //ikke fredag eller lørdag
+                mailTxt.append("dude, it is weekend, dont go to work tomorrow! RainSum=" + rainSumHoursOfInterest + "<br>\n");
+                return mailTxt.toString();
+            }
+
+            if (rainSumHoursOfInterest > 1.0) {
+                mailTxt.append(
+                    "Dude, it is raining more than 1.0mm, dont take the bike. rainSumHoursOfInterest=" + rainSumHoursOfInterest + "<br>\n");
+                return mailTxt.toString();
+            }
+
+            mailTxt.append(
+                "Dude, weather is fine next 2 days, rain is below 1.0mm. TAKE THE BIKE!! rainSumHoursOfInterest=" + rainSumHoursOfInterest +
+                    "<br>\n");
+
+        } catch (Exception e) {
+            mailTxt.append("Exception!! " + e + "<br>\n");
+        } finally {
+            if (mailTxt != null) {
+                MailClient.generateAndSendEmail(mailTxt);
+            }
+            mailTxt.append("Bye<br>\n<br>\n<br>\n<br>\n");
+            System.out.println(mailTxt);
+            return mailTxt.toString();
+
+        }
+    }
+
+    public static Double getRainSumHoursOfInterest(Document document) throws Exception {
+        int count = 0;
+        Double rainSum = 0.0;
+        Double rainSumHoursOfInterest = 0.0;
+        int numberOfTimesToGet = 30;
+
+        NodeList nodeList = document.getElementsByTagName("*");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+
+            Node node = nodeList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                if (node.getNodeName().equals("time")) {
+                    String periodFrom = node.getAttributes().item(0).getNodeValue();
+                    //String periodTo = node.getAttributes().item(1).getNodeValue();
+                    Double rainInPeriod = Double.valueOf(
+                        ((DeferredElementNSImpl) node).getElementsByTagName("precipitation").item(0).getAttributes().item(0).getNodeValue());
+                    rainSum += rainInPeriod;
+
+                    if (periodFrom.matches(".*T05.*|.*T06.*|.*T07.*|.*T08.*|.*T09.*|.*T13.*|.*T14.*|.*T15.*|.*T16.*|.*T17.*|.*T18.*")) {
+                        rainSumHoursOfInterest += rainInPeriod;
+                    }
+                    mailTxt.append(
+                        "count=" + count++ + ", periodFrom=" + periodFrom + ", rainInPeriod=" + rainInPeriod + ", rainSum=" + rainSum +
+                            ", rainSumHoursOfInterest=" + rainSumHoursOfInterest + "<br>\n");
+
+                    if (count > numberOfTimesToGet) {
+                        break;
+                    }
+
+                }
+            }
+        }
+        mailTxt.append("I have fetched data from YR from number of hours= " + count + ", rainSum=" + rainSum + ", rainSumHoursOfInterest=" +
+            rainSumHoursOfInterest + "<br>\n");
+        return rainSumHoursOfInterest;
+    }
+}
